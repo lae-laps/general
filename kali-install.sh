7f24e63f58152f406bdff423af212c6dbdfa270d
laelaps <77897569+lae-laps@users.noreply.github.com>
2021-09-18 08:21:28 -0400

    Add files via upload

---
+++ kali-install.sh
@@ -0,0 +1,22 @@
+read -p "password for root: " password
+mkdir tools
+cd tools
+git clone https://github.com/HACK3RY2J/Anon-SMS
+git clone https://github.com/umeshshinde19/instainsane
+git clone https://github.com/htr-tech/nexphisher
+git clone https://github.com/opsdisk/pagodo
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/Darkmux/SETSMS
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/Leeon123/CC-attack
+wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+echo "Updating APT"
+sudo -p $password apt update
+echo "Upgrading APT"
+sudo -p $password apt upgrade
+sudo dpkg -i nordvpn-release_1.0.0_all.deb
+sudo apt update
+rm nordvpn-release_1.0.0_all.deb
+sudo -p $password apt install nordvpn
+sudo -p $password apt install neofetch
+
